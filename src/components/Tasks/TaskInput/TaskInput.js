519979c4f55a9559760777b0000ae56afb6dfb5a
vlad-004 <prostoy-vlad@rambler.ru>
2023-02-03 01:36:58 +0200

    добавил динамические инлайн стили которые зависят от условий.

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,29 +1,43 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
 
 const TaskInput = (props) => {
-  const [inputText, setinputText] = useState("");
+    const [inputText, setInputText] = useState("");
+    const [isInputValid, setIsInputValid] = useState(true);
 
-  const taskInputChangeHandler = (event) => {
-    setinputText(event.target.value);
-  };
+    const taskInputChangeHandler = (event) => {
+        if (event.target.value.trim().length > 0) {
+            setIsInputValid(true);
+        }
+        setInputText(event.target.value);
+    };
 
-  const formSubmitHandler = (event) => {
-    event.preventDefault();
-    props.onAddTask(inputText);
-  };
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        if (inputText.trim().length === 0) {
+            setIsInputValid(false);
+            return;
+        }
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
-      </div>
-      <Button type="submit">Добавить Задачу</Button>
-    </form>
-  );
+        props.onAddTask(inputText);
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label style={{color: isInputValid ? 'black' : 'red'}}>Задачи</label>
+                <input style={
+                    {
+                        borderColor: isInputValid ? 'black' : 'red',
+                        background: isInputValid ? 'transparent' : 'salmon',
+                    }
+                } type="text" onChange={taskInputChangeHandler}/>
+            </div>
+            <Button type="submit">Добавить Задачу</Button>
+        </form>
+    );
 };
 
 export default TaskInput;
