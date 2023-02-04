28499389166416c19f1cfd33c2bbf8b45fddb58e
vlad-004 <prostoy-vlad@rambler.ru>
2023-02-04 15:14:49 +0200

    использование css модулей

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,39 +1,39 @@
-import "./Button.css";
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import styles from "./Button.module.css";
 
 //этот синтаксис стайл компонента вернет кнопку но со стилями которые мы укажем в обратных ковычках
-const Button = styled.button`
+// const Button = styled.button`
+//
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   color: white;
+//   background: #00358b;
+//   border-radius: 4px;
+//   box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
+//   cursor: pointer;
+//
+//   @media (min-width: 700px) {
+//     width: auto;
+//   }
+//
+//   &:focus {
+//     outline: none;
+//   }
+//
+//   &:hover,
+//   &:active {
+//     background: #245fbd;
+//     box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
+//   }
+// `;
 
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  color: white;
-  background: #00358b;
-  border-radius: 4px;
-  box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
-  cursor: pointer;
-
-  @media (min-width: 700px) {
-    width: auto;
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #245fbd;
-    box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
-  }
-`;
-
-// const Button = (props) => {
-//   return (
-//     <button type={props.type} className="button">
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
